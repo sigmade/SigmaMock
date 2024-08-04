347dfcdef0eebab86b42af6c42b1cf33c2e76001
Egor <sychov1991@mail.ru>
2024-08-04 23:23:11 +0500

    temo

--- ProxyTests/CheckServiceTests.cs
+++ ProxyTests/CheckServiceTests.cs
@@ -8,16 +8,18 @@ namespace ProxyTests
         [Fact]
         public async void ShouldValidProduct()
         {
+            // Arrange
             var product = new Product { Id = 22 };
             var mockProductService = new Mocker<IProductService>()
                 .SetupMethod(c => c.GetProduct(), product, 2)
-                .SetupMethod(c => c.SaveProduct(default), Task.FromResult(true), 1);
+                .SetupMethod(c => c.SaveProduct(default!), Task.FromResult(true), 1);
 
             var checkService = new CheckService(mockProductService.CreateInstance());
 
-
+            // Act
             var result = await checkService.IsValidProduct();
 
+            // Assert
             mockProductService.Verify();
             Assert.True(result);
         }
